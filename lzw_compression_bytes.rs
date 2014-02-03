2471667e1b0d3cc04fdfb69bbdadcf9ee2175c65
Brian Seitel <brian@rentjuice.com>
2014-02-03 09:47:42 -0800

    lzw

--- lzw_compression_bytes.rs
+++ lzw_compression_bytes.rs
@@ -2,37 +2,30 @@ use std::hashmap::HashMap;
 use std::io::File;
 
 fn main() {
-	// let uncompressed = ~"TOBEORNOTTOBEORTOBEORNOT";
 	let path = Path::new("/Users/brianseitel/Downloads/paradise-lost2.txt");
-	let uncompressed = File::open(&path).read_to_str();
-	println!("{:?}", uncompressed);
-	println!("{:?}", uncompressed.len());
+	let uncompressed = File::open(&path).read_to_end();
+
 	let compressed = compress(uncompressed.clone());
 	println!("{:?}", compressed);
 	let decompressed = decompress(compressed);
 	println!("{:?}", decompressed);
 }
 
-fn compress(uncompressed: ~str) -> ~[u32] {
-	let mut dict: HashMap<~str, u32> = HashMap::new();
+fn compress(uncompressed: ~[u8]) -> ~[u32] {
+	let maxdictsize = 1000;
+	let mut dict = build_compression_dict();
 
-	println!("boo");
-	for i in range(0, 256) {
-		let c = std::char::from_u32(i as u32).unwrap_or('?').to_str();
-		dict.insert(c.to_str(), i as u32);
-	}
-
-	println!("ha");
 	let mut w = ~"";
 	let mut result: ~[u32] = ~[];
-	for c in uncompressed.chars() {
+	for c in uncompressed.iter() {
+		if dict.len() > maxdictsize {
+			let mut dict = build_compression_dict();
+		}
 		let wc = w + c.to_str();
-		println!("foo {:?}", wc);
 
 		if dict.contains_key(&wc) {
 			w = wc.clone();
 		} else {
-			println!("baz {:?}", w);
 			let dictSize = dict.len();
 			result.push(*dict.get(&w));
 			dict.insert(wc.clone(), dictSize as u32);
@@ -46,18 +39,39 @@ fn compress(uncompressed: ~str) -> ~[u32] {
 	result
 }
 
-fn decompress(mut compressed: ~[u32]) -> ~str {
+fn build_compression_dict() -> HashMap<~str, u32> {
+	let mut dict: HashMap<~str, u32> = HashMap::new();
+
+	for i in range(0, 256) {
+		let c = std::char::from_u32(i as u32).unwrap_or('?').to_str();
+		dict.insert(c.to_str(), i as u32);
+	}
+
+	dict
+}
+
+fn build_decompression_dict() -> HashMap<u32, ~str> {
 	let mut dict: HashMap<u32, ~str> = HashMap::new();
 	for i in range(0, 256) {
 		let c = std::char::from_u32(i as u32).unwrap_or('?').to_str();
 		dict.insert(i as u32, c.to_str());
 	}
 
+	dict
+}
+
+fn decompress(mut compressed: ~[u32]) -> ~str {
+	let maxdictsize = 1000;
+	let mut dict = build_decompression_dict();
+
 	let mut dictSize: uint = dict.len();
 	let mut w: ~str = dict.get(&compressed[0]).to_str();
 	let mut result = w.clone();
 	compressed.remove(0);
 	for k in compressed.iter() {
+		if dict.len() > maxdictsize {
+			let mut dict = build_decompression_dict();
+		}
 		let mut entry: ~str = ~"";
 		if (dict.contains_key(k)) {
 			entry = dict.get(k).to_str();
@@ -65,10 +79,11 @@ fn decompress(mut compressed: ~[u32]) -> ~str {
 			entry = w.clone() + w.slice_to(1);
 		}
 
-		result = result + entry.clone();
-		dict.insert(dictSize as u32, w.clone() + entry.slice_to(1));
-		dictSize += 1;
-
+		result = result + entry;
+		if (entry.len() > 0) {
+			dict.insert(dictSize as u32, w + entry.slice_to(1));
+			dictSize += 1;
+		}
 		w = entry;
 	}
 
---
+++ lzw_compression_bytes_wip.rs
@@ -0,0 +1,116 @@
+use std::hashmap::HashMap;
+use std::io::File;
+
+fn main() {
+	let path = Path::new("/Users/brianseitel/Downloads/paradise-lost2.txt");
+	let uncompressed = File::open(&path).read_to_end();
+
+	let mut uncompressed = (~"TOBEORNOTTOBEORTO BEORNOT").into_bytes();
+	println!("Original: {:?}", uncompressed.len());
+	println!("Compressing...");
+	let compressed = compress(uncompressed);
+	println!("Done compressing!");
+	println!("Compressed: {:?}", compressed.len());
+	println!("Writing to file...");
+	let path2 = Path::new("test.txt.compressed");
+	let mut dupe = File::create(&path2);
+	dupe.write(compressed);
+	println!("Done");
+
+	// Decompress
+
+	println!("Decompressing!");
+	let decompressed = decompress(compressed);
+	println!("Done decompressing!");
+	println!("Decompressed: {:?}", decompressed.len());
+
+	println!("Writing to file...");
+	let path3 = Path::new("test.txt.decompressed");
+	let mut dupe = File::create(&path3);
+	dupe.write(decompressed);
+	println!("Done!");
+
+}
+
+fn compress(uncompressed: ~[u8]) -> ~[u8] {
+	let maxdictsize = 1000;
+	let mut dict = build_compression_dict();
+
+	let mut w: ~[u8] = ~[];
+	let mut result: ~[u8] = ~[];
+	for &c in uncompressed.iter() {
+		if dict.len() > maxdictsize {
+			let mut dict = build_compression_dict();
+		}
+		let mut wc = w.clone();
+		wc.push(c);
+
+		if dict.contains_key(&wc) {
+			w = wc.clone();
+		} else {
+			println!("Adding {:?} => {:?}", wc, dict.get(&w));
+			let dictSize = dict.len();
+			result.push(*dict.get(&w));
+			dict.insert(wc.clone(), dictSize as u8);
+			w = ~[c];
+		}
+	}
+
+	if w.len() > 0 {
+		result.push(*dict.get(&w));
+	}
+	result
+}
+
+fn build_compression_dict() -> HashMap<~[u8], u8> {
+	let mut dict: HashMap<~[u8], u8> = HashMap::new();
+
+	for i in range(0, 256) {
+		let c = std::char::from_u32(i as u32).unwrap_or('?');
+		dict.insert(~[c as u8], i as u8);
+	}
+
+	dict
+}
+
+fn build_decompression_dict() -> HashMap<u8, ~[u8]> {
+	let mut dict: HashMap<u8, ~[u8]> = HashMap::new();
+	for i in range(0, 256) {
+		let c = std::char::from_u32(i as u32).unwrap_or('?');
+		dict.insert(i as u8, ~[c as u8]);
+	}
+
+	dict
+}
+
+fn decompress(mut compressed: ~[u8]) -> ~[u8] {
+	let maxdictsize = 1000;
+	let mut dict = build_decompression_dict();
+
+	let mut dictSize: uint = dict.len();
+	let mut w: ~[u8] = dict.get(&compressed[0]).to_owned();
+	let mut result = w.clone();
+	compressed.remove(0);
+	for k in compressed.iter() {
+		if dict.len() > maxdictsize {
+			let mut dict = build_decompression_dict();
+		}
+		let mut entry: ~[u8] = ~[];
+		if (dict.contains_key(k)) {
+			entry = dict.get(k).to_owned();
+		} else if k == &((dictSize - 1) as u8) {
+			entry.push(w[0]);
+		}
+
+		std::vec::bytes::push_bytes(&mut result, entry);
+		if (entry.len() > 0) {
+			let mut new_entry = w.clone();
+			new_entry.push(w[0]);
+			dict.insert(dictSize as u8, new_entry);
+			dictSize += 1;
+		}
+		w = entry;
+	}
+
+	result
+}
