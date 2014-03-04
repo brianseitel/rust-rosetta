826d89e20957fe593e6eb7e5320f3a2f4d88fede
Brian Seitel <brian@rentjuice.com>
2014-03-04 15:15:36 -0800

    add decode method

--- run_length.rs
+++ run_length.rs
@@ -2,6 +2,9 @@ fn main() {
 	let message: ~str = ~"WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWBWWWWWWWWWWWWWW";
 	let results = run_length(message);
 	println!("{}", results);
+
+	let results = decode(results);
+	println!("{}", results);
 }
 
 fn run_length(input: ~str) -> ~str {
@@ -33,6 +36,29 @@ fn run_length(input: ~str) -> ~str {
 	output
 }
 
+fn decode(input: ~str) -> ~str {
+	let mut output = ~"";
+	let mut number = ~"";
+
+	// Loop through each character
+	for c in input.chars() {
+
+		// If the character is a digit, append it to the number string.
+		// Eventually, we'll wind up with "1" or "12"
+		if c.is_digit() {
+			number = number + c.to_str();
+		} else {
+			// If it's not a digit, then it's a letter. Cast to uint,
+			// then repeat the character X times and append to output.
+			let num = from_str::<uint>(number).unwrap_or(0);
+			output = output + c.to_str().repeat(num);
+			number = ~"";
+		}
+	}
+
+	output
+}
+
 #[test]
 fn test_run_length() {
 	let message = ~"WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWBWWWWWWWWWWWWWW";
@@ -40,5 +66,15 @@ fn test_run_length() {
 
 	if run_length(message) != output {
 		fail!("Output does not match message!");
+	}
+}
+
+#[test]
+fn test_run_length_decode() {
+	let encoded = ~"12W1B12W3B24W1B14W";
+	let output = ~"WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWBWWWWWWWWWWWWWW";
+
+	if decode(encoded) != output {
+		fail!("Output does not match encoded message!");
 	}
 }
